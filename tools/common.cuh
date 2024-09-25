52952f64a2a4072b1e2e50ed7893e80938b4142f
yzxcxy <yzxcxyshabao@gmail.com>
2024-09-25 11:18:46 +0800

    complete matrix_sum

---
+++ tools/common.cuh
@@ -0,0 +1,28 @@
+#pragma once
+#include <stdlib.h>
+#include <stdio.h>
+
+void setGPU(){
+        //检测设备中GPU的数量
+    int deviceCount=0;
+    cudaError_t cuda_error=cudaGetDeviceCount(&deviceCount);
+
+    //判断错误情况
+    if(cuda_error!=cudaSuccess || deviceCount==0){
+        printf("cudaGetDeviceCount failed!  Do you have a CUDA-Capable GPU installed?\n");
+        exit(-1);
+    }else{
+        printf("CUDA Device Query...\n");
+        printf("There are %d CUDA devices.\n",deviceCount);
+    }
+
+    //设置0号设备执行
+    int index_device=0;
+    cuda_error=cudaSetDevice(index_device);
+    if(cuda_error!=cudaSuccess){
+        printf("cudaSetDevice failed!  Do you have a CUDA-Capable GPU installed?\n");
+        exit(-1);
+    }else{
+        printf("Set device %d to execute.\n",index_device);
+    }
+}
